998fec3913eea7baed490b0ae3818b3f053f326f
duc chinh <ducchinhcp882002@gmail.com>
2025-12-26 09:46:22 +0800

    [PC] update parking spots schema

--- src/database/schemas/parking_spots.chema.ts
+++ src/database/schemas/parking_spots.chema.ts
@@ -2,6 +2,7 @@ import { Prop, Schema } from "@nestjs/mongoose";
 import { MongoBaseSchema } from "./base.schema";
 import { createSchemaForClass } from "../utils/helper";
 import { MongoCollection } from "../utils/constants";
+import * as mongoose from "mongoose";
 export type Parking_SpotsDocument = SchemaDocument<Parking_Spots>;
 @Schema({
     timestamps: true,
@@ -15,7 +16,11 @@ export type Parking_SpotsDocument = SchemaDocument<Parking_Spots>;
 })
 export class Parking_Spots extends MongoBaseSchema {
     @Prop({ required: true, type: String })
-    spotName: string;
+    spotCode: string; //A1-1, A1-2
+    @Prop({ required: true, type: Boolean, default: true })
+    isAvailable: boolean;
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Parking_Lots' })
+    parkingLotId: mongoose.Types.ObjectId;
 }
 
 const Parking_SpotsSchema = createSchemaForClass(Parking_Spots);
