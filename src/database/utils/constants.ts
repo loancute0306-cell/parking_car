af8c5e8aa94c11bcea4684499d8e595078ba0ddd
duc chinh <ducchinhcp882002@gmail.com>
2025-12-27 11:25:28 +0800

    [PC] add mongo collection car info

--- src/database/utils/constants.ts
+++ src/database/utils/constants.ts
@@ -2,5 +2,5 @@ export enum MongoCollection {
     USERS = 'users',
     PARKING_LOTS = 'parking_lots',
     PARKING_SPOTS = 'parking_spots',
-    LICENSE_PLATES = 'license_plates',
+    CAR_INFO = 'car_info',
 }
